350754b6b88a292beede7bc3eb8c82b4f331a846
thuan <pham.t.286.01@gmail.com>
2024-10-07 14:50:20 +0900

    PWA implement

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,27 @@ import SessionProvider from "./Provider";
 import { Toaster } from "@/components/ui/toaster";
 import { Zen_Maru_Gothic } from "next/font/google";
 import { M_PLUS_Rounded_1c } from "next/font/google";
+import { Html, Main, Head } from "next/document";
 
-const courier = Zen_Maru_Gothic({ subsets: ["latin"], weight: ["400", "700"] });
-const m_plus = M_PLUS_Rounded_1c({ subsets: ["latin"], weight: ["400", "700"] });
-const inter = Inter({ subsets: ["latin"] });
+// const courier = Zen_Maru_Gothic({ subsets: ["latin"], weight: ["400", "700"] });
+// const m_plus = M_PLUS_Rounded_1c({
+//   subsets: ["latin"],
+//   weight: ["400", "700"],
+// });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "漢字学び",
   description: "A Thuan's product",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "next14", "pwa", "next-pwa"],
+  // viewport:
+  //   "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "dictionary-icon", url: "/icon-128.png" },
+    { rel: "icon", url: "/icon-128.png" },
+  ],
 };
 
 export default function RootLayout({
@@ -22,8 +35,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-512.png"></link>
+      </head>
       <SessionProvider>
-        <body className={m_plus.className}>
+        <body >
           <main>{children}</main>
           <Toaster />
         </body>
